f6b862fa3a5627a2abd0a1a924ba7f794aa918b3
soum <sman987656789@gmail.com>
2023-04-26 10:05:35 +0530

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Box } from '@chakra-ui/react';
 import Home from './Pages/Home';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <Box>
       <Home/>
     </Box>
+    </BrowserRouter>
   );
 }
 
